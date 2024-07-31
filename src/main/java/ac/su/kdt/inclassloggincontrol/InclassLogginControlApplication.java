ee738708e599a96fcfd70a27b1607b1b1b0f24fb
saya61 <msaki061@gmail.com>
2024-07-31 15:14:54 +0900

    logging

---
+++ src/main/java/ac/su/kdt/inclassloggincontrol/InclassLogginControlApplication.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.inclassloggincontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclassLogginControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InclassLogginControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/kdt/inclassloggincontrol/InclassLogginControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.inclassloggincontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InclassLogginControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
